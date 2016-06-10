255c072a6e954385f6601b7f87c2d124117f70f9
tj2001 <jung.m.tim@gmail.com>
2016-06-10 11:00:19 -0700

    added README

---
+++ README.md
@@ -0,0 +1,34 @@
+# _PingPong_
+
+#### _It will generate a list of numbers and strings designated by original input number, {June 10, 2016}_
+
+#### By _**Tim Jung**_
+
+## Description
+
+_The purpose of this web application is to pass a class code review in Epicodus. It will give the word ping for every number with a factor of three and pong for a number with a factor of five._
+
+## Setup/Installation Requirements
+
+* _Download zip or clone files to setup on your desktop._
+* _Open index.html file in a Chrome browser._
+
+_Everything should just work fine, if directions are followed._
+
+## Known Bugs
+
+_It's mostly bug free. Older browsers might have issues._
+
+## Support and contact details
+
+_Feel free to make a branch and work on it._
+
+## Technologies Used
+
+_HTML, Javascript, and JQuery were used._
+
+### License
+
+*This is created under CC.*
+
+CC Copyright (c) 2016
--- js/scripts.js
+++ js/scripts.js
@@ -2,86 +2,32 @@ $(document).ready(function(){
 // debugger;
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("#number").val());
+    var inputnumber = parseInt($("#number").val());
     var narray = [];
 
-    for(var index = 1; index <= number; index += 1){
+    for(var index = 1; index <= inputnumber; index += 1){
       narray.push(index);
     }
 
     for(var pp = 2; pp < narray.length; pp++) {
       for(var i=0; i < narray.length; i++){
-        var px = narray[i];
-        if(px%3 === 0 && px != 0){
+        var outnumber = narray[i];
+        if(outnumber%3 === 0 && outnumber != 0){
           narray[i] = "ping";
         }
-        if(px%5 === 0 && px != 0){
+        if(outnumber%5 === 0 && outnumber != 0){
           narray[i] = "pong";
         }
-        if(px%15 === 0 && px != 0){
+        if(outnumber%15 === 0 && outnumber != 0){
           narray[i] = "pingpong";
         }
-        // // if(px%pp === 0 && px!=pp) {
-        // //   narray.splice(i,1);
-        // }
       }
     }
+    document.getElementById("result").innerHTML = "";
+    narray.forEach(function(sort){
+    $("#result").append("<li>" + sort + "</li>");
+  });
 
-    $("#result").text(narray.join(" "));
 
   });
 });
-
-
-//
-//   $("form#factorial").submit(function(event) {
-//   event.preventDefault();
-//   var number = parseInt($("#number").val());
-//   var out = 1;
-//
-//   if (number < 0 || isNaN(number)){
-//     $("#result").text("this will not work")
-//   } else {
-//     for(var beg = 1; beg <= number; beg += 1){
-//       out = out * beg;
-//     }
-//     $("#result").text(out.toString());
-//   }
-// });
-//
-//
-//   var arrVowel = ["a", "e", "o", "u", "i"];
-//   var newArr = word.split('');
-//
-// arr.push()
-//
-// for (var i = 0; i < arrVowel.length; i++) {
-//   if (word.charAt(0) === arrVowel[i]) {
-//     var result = true;
-//     break;
-//   } else {
-//     var result = false
-//   }
-// }
-//
-//
-//     if (result === true) {
-//       var joinArr =newArr.join('');
-//       var output = joinArr + "ay";
-//
-//
-//       // return output;
-//
-//     } else {
-//       newArr.shift();
-//       newArr.push(word.charAt(0));
-//       var joinArr = newArr.join("");
-//       var output = joinArr + "ay";
-//       // return output;
-//     }
-//     return output;
-//
-//
-//
-// };
-//
