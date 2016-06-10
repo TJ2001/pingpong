4f60ff0768dc88707ad95078f87c11a3bef98798
tj2001 <jung.m.tim@gmail.com>
2016-06-10 09:24:48 -0700

    got it to give out sequenced numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,78 @@
-
-var pigLatin = function(word) {
-
-  var arrVowel = ["a", "e", "o", "u", "i"];
-  var newArr = word.split('');
-
-for (var i = 0; i < arrVowel.length; i++) {
-  if (word.charAt(0) === arrVowel[i]) {
-    var result = true;
-    break;
-  } else {
-    var result = false
-  }
-}
-
-
-    if (result === true) {
-      var joinArr =newArr.join('');
-      var output = joinArr + "ay";
-
-
-      // return output;
-
-    } else {
-      newArr.shift();
-      newArr.push(word.charAt(0));
-      var joinArr = newArr.join("");
-      var output = joinArr + "ay";
-      // return output;
-    }
-    return output;
-
-
-
-};
-
-
-$(document).ready(function() {
-  $("form#pig-latin").submit(function(event) {
+$(document).ready(function(){
+// debugger;
+  $("form#pingpong").submit(function(event) {
     event.preventDefault();
-    word = $("input#word").val();
-    var result = pigLatin(word);
+    var number = parseInt($("#number").val());
+    var narray = [];
 
+    for(var index = 0; index <= number; index += 1){
+      narray.push(index);
+    }
 
+    for(var pp = 2; pp < narray.length; pp++) {
+      for(var i=0; i < narray.length; i++){
+        var px = narray[i];
+        // // if(px%pp === 0 && px!=pp) {
+        // //   narray.splice(i,1);
+        // }
+      }
+    }
 
-    $("#result").text(result);
+    $("#result").text(narray.join(", "));
+
   });
 });
+
+
+//
+//   $("form#factorial").submit(function(event) {
+//   event.preventDefault();
+//   var number = parseInt($("#number").val());
+//   var out = 1;
+//
+//   if (number < 0 || isNaN(number)){
+//     $("#result").text("this will not work")
+//   } else {
+//     for(var beg = 1; beg <= number; beg += 1){
+//       out = out * beg;
+//     }
+//     $("#result").text(out.toString());
+//   }
+// });
+//
+//
+//   var arrVowel = ["a", "e", "o", "u", "i"];
+//   var newArr = word.split('');
+//
+// arr.push()
+//
+// for (var i = 0; i < arrVowel.length; i++) {
+//   if (word.charAt(0) === arrVowel[i]) {
+//     var result = true;
+//     break;
+//   } else {
+//     var result = false
+//   }
+// }
+//
+//
+//     if (result === true) {
+//       var joinArr =newArr.join('');
+//       var output = joinArr + "ay";
+//
+//
+//       // return output;
+//
+//     } else {
+//       newArr.shift();
+//       newArr.push(word.charAt(0));
+//       var joinArr = newArr.join("");
+//       var output = joinArr + "ay";
+//       // return output;
+//     }
+//     return output;
+//
+//
+//
+// };
+//
